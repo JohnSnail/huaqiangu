bdf6ee843555c73b963aca104445b9da3657d1cc
郭江伟 <Jason_name@163.com>
2015-07-18 00:02:37 +0800

    Initial Commit

---
+++ huaqiangu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  huaqiangu
+//
+//  Created by Jiangwei on 15/7/18.
+//  Copyright (c) 2015年 Jiangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ huaqiangu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  huaqiangu
+//
+//  Created by Jiangwei on 15/7/18.
+//  Copyright (c) 2015年 Jiangwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
