841f39ff09f4121a3429c05822f9ef69f5c588a7
jasonG <jason_name@163.com>
2016-03-02 16:25:58 +0800

    冲突撤回

--- huaqiangu/Controller/DownController.m
+++ huaqiangu/Controller/DownController.m
@@ -71,14 +71,13 @@
 -(void)downAction{
     NSLog(@"下载");
     [self.downMuArray enumerateObjectsWithOptions:NSEnumerationConcurrent usingBlock: ^(TrackModel *track,NSUInteger idx, BOOL *stop){
-        track.downStatus = @"done";
-        [[MainList sharedManager] mergeWithContent:track];
-        
-        [self download:track.playUrl64 progressLabel:nil progressView:nil button:nil];
+        if (track.isSelected) {
+            track.downStatus = @"done";
+            [[MainList sharedManager] mergeWithContent:track];
+            [self download:track.playUrl64 progressLabel:nil progressView:nil button:nil];
+        }
     }];
 }
-
-
 
 #pragma mark - tableview代理方法
 
